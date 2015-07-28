5acdb7a8ddbdb21b50f85d6b11d839738b1955b6
genie9 <jenny.lyjina@gmail.com>
2015-07-28 14:28:59 +0300

    avattu RunkoAppin kansiorakennetta; action bar lisätty, buginen

---
+++ app/src/main/java/runko/runkoandroidapp/RunkoActivity.java
@@ -0,0 +1,49 @@
+package runko.runkoandroidapp;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+public class RunkoActivity extends android.support.v7.app.ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_view_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+            switch (item.getItemId()) {
+                case R.id.action_main_window:
+                    ActionBarActivity.gotoMain();
+                    return true;
+                case R.id.action_content:
+                    ActionBarActivity.openContent();
+                    return true;
+                case R.id.action_manager:
+                    ActionBarActivity.openManager();
+                    return true;
+                case R.id.action_user:
+                    ActionBarActivity.openUserSettings();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+    }
+}
