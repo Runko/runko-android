d67f01fc3069a1d156f9f7d3d8ead1f9a0c0ac90
genie9 <jenny.lyjina@gmail.com>
2015-07-29 11:50:03 +0300

    RunkoandroidApp takaisin

---
+++ RunkoAndroidApp/app/src/main/java/runko/runkoandroidapp/ActionBarActivity.java
@@ -0,0 +1,21 @@
+package runko.runkoandroidapp;
+
+/**
+ * Created by evly on 28/07/15.
+ */
+public class ActionBarActivity {
+    public static void openFavorites() {
+    }
+
+    public static void openUserSettings() {
+    }
+
+    public static void openManager() {
+    }
+
+    public static void openContent() {
+    }
+
+    public static void gotoMain() {
+    }
+}
